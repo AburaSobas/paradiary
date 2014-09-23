509a11d1457d98c06fc2b0670b547f031b8e7dbb
skipchibi <takase@yu-r.net>
2014-09-23 21:18:34 +0900

    ルーティングにMongoDBを追加

--- paradiary/webapp/routes.js
+++ paradiary/webapp/routes.js
@@ -12,7 +12,22 @@
 
 // ------------ BEGIN MODULE SCOPE VARIABLES --------------
 'use strict';
-var configRoutes;
+var 
+  configRoutes,
+  mongodb     = require( 'mongodb' ),
+  
+  mongoServer = new mongodb.Server(
+    'localhost',
+    mongodb.Connection.DEFAULT_PORT
+  ),
+  dbHandle    = new mongodb.Db(
+    'paradiary', mongoServer, { safe : true }
+  );
+
+dbHandle.open( function () {
+  console.log( '** Connected to MongoDB **' );
+});
+  
 // ------------- END MODULE SCOPE VARIABLES ---------------
 
 configRoutes = function ( app, server ) {
